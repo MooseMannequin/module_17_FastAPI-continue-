2dd3bfaac8c03511d8838cddfcd8f1ddf6186195
MooseMannequin <EugeneNeudene@yandex.ru>
2025-01-20 18:46:31 +0300

    Update task.py

--- 17_2/app/models/task.py
+++ 17_2/app/models/task.py
@@ -1,7 +1,7 @@
-from fastapi_module.module_17.app.backend.db import Base
+from app.backend.db import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
-from fastapi_module.module_17.app.models import *
+from app.models import *
 
 class Task(Base):
     __tablename__ = 'tasks'
